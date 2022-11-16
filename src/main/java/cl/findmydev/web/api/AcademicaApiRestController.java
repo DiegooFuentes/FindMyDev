a20b0b2ea9bdf57b1de747b2a83aa9a2aa5c8e13
vale <valcarcamo03@gmail.com>
2022-11-16 11:36:22 -0300

    package y academica

---
+++ src/main/java/cl/findmydev/web/api/AcademicaApiRestController.java
@@ -0,0 +1,22 @@
+package cl.findmydev.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.findmydev.web.models.Academica;
+import cl.findmydev.web.services.AcademicaServiceImpl;
+
+@RestController
+public class AcademicaApiRestController {
+	@Autowired
+	private AcademicaServiceImpl academicaServiceImpl;
+	
+	//http://localhost:8080/guardar/academica
+	@RequestMapping("/guardar/academica")
+	public Academica guardarAcademica(@RequestBody Academica academica) {
+		return academicaServiceImpl.guardarAcademica(academica);
+	}
+	
+}
